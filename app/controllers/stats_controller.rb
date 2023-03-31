c4a847cf592b3fb6d92ebed4409532fb109f8829
Javier <javierdiazt406@gmail.com>
2023-03-30 23:04:26 -0400

    First View of Stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,11 @@
 class StatsController < ApplicationController
   def index 
     render json: {
-      
+      all: Rifa.count,
+      actives: Rifa.active.count,
+      send: Rifa.send.count,
+      expired: Rifa.expired.count,
+      status_code: 200
     }
   end
 end
