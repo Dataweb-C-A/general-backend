9f1c28cd301b94dd3d1358a8663dc303efceefb3
Javier <javierdiazt406@gmail.com>
2023-04-03 02:12:10 -0400

    Add validations to generator

--- app/sidekiq/generate_rifa_tickets_job.rb
+++ app/sidekiq/generate_rifa_tickets_job.rb
@@ -1,69 +1,93 @@
-class GenerateRifaTicketsJob
-  include Sidekiq::Job
-
-  ZODIAC_SIGNS = [
-    'Aries',
-    'Tauro',
-    'Geminis',
-    'Cancer',
-    'Leo',
-    'Virgo',
-    'Libra',
-    'Escorpio',
-    'Sagitario',
-    'Capricornio',
-    'Acuario',
-    'Piscis'
-  ].freeze
-
-  SPORTS_WILDCARDS = [
-    'Baloncesto',
-    'Beisbol',
-    'Futbol',
-    'Voleibol',
-    'Playa',
-    'Golf',
-    'Futbol Americano',
-    'Tenis',
-    'Billar',
-    'Bowling',
-    'Ping Pong',
-    'Hockey',
-  ].freeze
-
+class GenerateRifaTicketsJob < ApplicationJob
   def generate(rifa_id)
     rifa = Rifa.find_by(id: rifa_id)
 
     return unless rifa
 
     ActiveRecord::Base.transaction do
-      rifa.update(serial: SecureRandom.hex(5),
-                  expired: rifa.rifDate + 5.days,
-                  tickets_type: rifa.loteria === 'Zulia 7A' ? 'Signs' : 'Wildcards')
-
-      if (rifa.loteria === 'Zulia 7A')
-        ZODIAC_SIGNS.each_with_index do |sign, index|
-          RifaTicket.create(rifa_id: rifa_id,
-                            serial: SecureRandom.hex(5),
-                            sign: sign,
-                            ticket_nro: index + 1,
-                            number: rifa.numbers,
-                            is_sold: false)
-        end
-        Rails.logger.info 'Zulia 7A tickets seeded!'
-      elsif (rifa.loteria === 'Triple Pelotica')
-        SPORTS_WILDCARDS.each_with_index do |sport, index|
-          RifaTicket.create(rifa_id: rifa_id,
-                            sign: sport,
-                            serial: SecureRandom.hex(5),
-                            ticket_nro: index + 1,
-                            number: rifa.numbers,
-                            is_sold: false)
-        end
-        Rails.logger.info 'Triple Pelotica tickets seeded!'
-      else
-        Rails.logger.info "Unsupported loteria: #{rifa.loteria}"
+      if rifa.rifa_tickets.count >= 12
+        logger.info 'Cannot generate tickets for this rifa, tickets already generated!'
+        ActiveRecord::Rollback
+        return
       end
+
+      if rifa.loteria === 'Zulia 7A'
+        GenerateZulia7ATicketsService.new(rifa).call
+        logger.info 'Zulia 7A tickets seeded!'
+      elsif rifa.loteria === 'Triple Pelotica'
+        GenerateTriplePeloticaTicketsService.new(rifa).call
+        logger.info 'Triple Pelotica tickets seeded!'
+      else
+        logger.info "Unsupported loteria: #{rifa.loteria}"
+        Rifa.destroy(rifa.id)
+        return ActiveRecord::Rollback
+      end
+    end
+  end
+end
+
+class GenerateZulia7ATicketsService
+  def initialize(rifa)
+    @rifa = rifa
+    @signs = [
+      'Aries',
+      'Tauro',
+      'Geminis',
+      'Cancer',
+      'Leo',
+      'Virgo',
+      'Libra',
+      'Escorpio',
+      'Sagitario',
+      'Capricornio',
+      'Acuario',
+      'Piscis'
+    ]
+  end
+
+  def call
+    @rifa.update(serial: SecureRandom.hex(5),
+                 expired: @rifa.rifDate + 5.days,
+                 tickets_type: 'Signs')
+    @signs.each_with_index do |sign, index|
+      RifaTicket.create(rifa_id: @rifa.id,
+                        serial: SecureRandom.hex(5),
+                        sign: sign,
+                        ticket_nro: index + 1,
+                        number: @rifa.numbers)
+    end
+  end
+end
+
+class GenerateTriplePeloticaTicketsService
+  def initialize(rifa)
+    @rifa = rifa
+    @sports_wildcards = [
+      'Baloncesto',
+      'Beisbol',
+      'Futbol',
+      'Voleibol',
+      'Playa',
+      'Golf',
+      'Futbol Americano',
+      'Tenis',
+      'Billar',
+      'Bowling',
+      'Ping Pong',
+      'Hockey',
+    ]
+  end
+
+  def call
+    @rifa.update(serial: SecureRandom.hex(5),
+                 expired: @rifa.rifDate + 5.days,
+                 tickets_type: 'Wildcards')
+    @sports_wildcards.each_with_index do |sport, index|
+      RifaTicket.create(rifa_id: @rifa.id,
+                        serial: SecureRandom.hex(5),
+                        sign: sport,
+                        ticket_nro: index + 1,
+                        number: @rifa.numbers)
     end
   end
 end
