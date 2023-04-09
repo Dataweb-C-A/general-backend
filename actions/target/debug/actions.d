c3b9bd6d3c18d56247bbacbc596fdf985c78603d
Javier <javierdiazt406@gmail.com>
2023-04-09 14:55:58 -0400

    Delete build

--- actions/target/debug/actions.d
+++
@@ -1,1 +0,0 @@
-/home/javier/work/dataweb/cda_backend/actions/target/debug/actions: /home/javier/work/dataweb/cda_backend/actions/src/main.rs
--- actions/target/debug/deps/actions-9c8ae091026cddf8.d
+++
@@ -1,5 +0,0 @@
-/home/javier/work/dataweb/cda_backend/actions/target/debug/deps/actions-9c8ae091026cddf8: src/main.rs
-
-/home/javier/work/dataweb/cda_backend/actions/target/debug/deps/actions-9c8ae091026cddf8.d: src/main.rs
-
-src/main.rs:
