d71406d8601cf10d15d60614ec210ec2558d7dfb
Javier Diaz <javierdiazt406@gmail.com>
2023-04-19 22:50:02 -0400

    Fixing draws

--- db/migrate/20230419234805_create_draws.rb
+++ db/migrate/20230419234805_create_draws.rb
@@ -1,7 +1,7 @@
 class CreateDraws < ActiveRecord::Migration[7.0]
   def change
     create_table :draws do |t|
-      t.string :title,
+      t.string :title
       t.string :first_prize
       t.string :second_prize
       t.string :uniq
