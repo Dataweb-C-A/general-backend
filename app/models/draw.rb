ff3475cc7ae776a68580f16fcbc55913d9020948
Javier Diaz <javierdiazt406@gmail.com>
2023-05-05 22:28:45 -0400

    Draw model validations

--- app/models/draw.rb
+++ app/models/draw.rb
@@ -31,6 +31,62 @@ class Draw < ApplicationRecord
 
   after_create :generate_places
 
+  validates :title,
+            presence: true,
+            length: { minimum: 5, maximum: 50 }
+
+  validates :first_prize,
+            presence: true,
+            length: { minimum: 5, maximum: 50 }
+  
+  validates :second_prize,
+            length: { minimum: 5, maximum: 50 }
+
+  validates :init_date,
+            presence: true
+            greater_than_or_equal_to: Date.today
+  
+  validates :expired_date,
+            greater_than_or_equal_to: :init_date
+
+  validates :numbers,
+            presence: true,
+            numericality: { 
+                            only_integer: true, 
+                            greater_than_or_equal_to: 1, 
+                            less_than_or_equal_to: 999 
+                          }
+
+  validates :tickets_count,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 100 }
+
+  validates :loteria,
+            presence: true,
+            length: { minimum: 5, maximum: 50 }
+          
+  validates :draw_type,
+            presence: true,
+            inclusion: { in: %w[Progressive End-To-Date] }
+
+  validates :limit,
+            presence: false,
+            numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+          
+  validates :price_unit,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0.1 }
+
+  validates :money,
+            presence: true,
+            inclusion: { in: %w[BsF $ COL] }
+      
+  validates :visible_taquillas_ids,
+            presence: true,
+            
+  validates :automatic_taquillas_ids,
+            presence: true,
+
   scope :active, -> { where('is_active = true') }
   scope :expired, -> { where('is_active = false') }
 
@@ -42,7 +98,7 @@ class Draw < ApplicationRecord
     return unless current 
     
     roles = {
-      'Admin': -> { act}
+      'Admin': -> { Draw.all.count }
     }
   end
 end
