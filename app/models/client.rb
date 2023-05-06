efbe5142e7d79b498e2c05a95955427895fe36d9
Javier Diaz <javierdiazt406@gmail.com>
2023-05-05 23:36:44 -0400

    Client added

--- app/models/client.rb
+++ app/models/client.rb
@@ -13,8 +13,26 @@
 class Client < ApplicationRecord
   has_many :places, dependent: :destroy
 
-  validates :dni, presence: true, uniqueness: true, length: { minimum: 6, maximum: 10 }, inclusion: { in: %w(V E) }
-  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :phone, presence: true, length: { minimum: 11, maximum: 11 }, numericality: { only_integer: true }, uniqueness: true
-  validates :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :dni, 
+            presence: true, 
+            uniqueness: true, 
+            length: { minimum: 6, maximum: 10 }, 
+            inclusion: { in: %w(V E) }
+
+  validates :name, 
+            presence: true, 
+            length: { minimum: 3, maximum: 50 }
+
+  validates :phone, 
+            presence: true, 
+            length: { minimum: 11, maximum: 11 }, 
+            numericality: { only_integer: true }, 
+            uniqueness: true, 
+            format: { with: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/ }
+
+  validates :email, 
+            presence: true, 
+            uniqueness: true, 
+            length: { minimum: 5, maximum: 50 }, 
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 end
