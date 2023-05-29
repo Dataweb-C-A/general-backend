22149125a4d3887a0161ff245795f5433b8a4691
Javier Diaz <javierdiazt406@gmail.com>
2023-05-29 09:04:15 -0400

    Backend finished endpoint

--- app/controllers/draws_controller.rb
+++ app/controllers/draws_controller.rb
@@ -7,11 +7,15 @@ class DrawsController < ApplicationController
   end
 
   def public_get
-    if params[:user_id].present?
-      @draws = Draw.find_by(owner_id: params[:user_id])
-      render json: [@draws], status: :ok
+    if (params[:user_id].present? && request.headers[:Authorization].present?)
+      if (Whitelist.find_by(user_id: params[:user_id]) && request.headers[:Authorization] == 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzeXN0ZW0iOiJyaWZhbWF4Iiwic2VjcmV0IjoiZjJkN2ZhNzE3NmE3NmJiMGY1NDI2ODc4OTU5YzRmNWRjMzVlN2IzMWYxYzE1MjYzNThhMDlmZjkwYWE5YmFlMmU4NTc5NzM2MDYzN2VlODBhZTk1NzE3ZjEzNGEwNmU1NDIzNjc1ZjU4ZDIzZDUwYmI5MGQyNTYwNjkzNDMyOTYiLCJoYXNoX2RhdGUiOiJNb24gTWF5IDI5IDIwMjMgMDg6NTE6NTggR01ULTA0MDAgKFZlbmV6dWVsYSBUaW1lKSJ9.ad-PNZjkjuXalT5rJJw9EN6ZPvj-1a_5iS-2Kv31Kww')
+        @draws = Draw.find_by(owner_id: params[:user_id])
+        render json: [@draws], status: :ok
+      else
+        render json: { message: 'No autorizado' }, status: :forbidden
+      end
     else
-      render json: { message: 'No se encontraron rifas' }, status: :not_found
+      render json: { message: 'No autorizado' }, status: :forbidden
     end
   end
 
