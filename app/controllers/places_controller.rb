fc072f6238f7214a4ec392c45eaa5ffd626b9988
Javier Diaz <javierdiazt406@gmail.com>
2023-06-23 10:03:23 -0400

    Push

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -49,69 +49,28 @@ class PlacesController < ApplicationController
     @draw = Draw.find(params[:draw_id])
     @place = Place.find(params[:plays])
     @agency = Whitelist.find(@draw.owner_id)
+    @client = Client.find(@place.client_id)
     
-    @eighty_mm = "	    RIFAMAX
----------------------------------
-            NUMEROS
-#{@place.place_numbers.to_s.tr('[]', '')}
----------------------------------
-            PREMIOS
-#{@draw.first_prize} #{}
----------------------------------
-Precio:       	  #{@draw.price_unit}0$
-Tipo:	          Terminal(00-99)
-Agencia:    	  #{@agency.name}
-Ticket numero:    #{@draw.numbers}
-Fecha de venta:   #{@place.created_at.strftime("%d/%m/%Y %H:%M")}
-Tipo sorteo:	  #{@draw.draw_type}
-Fecha sorteo: 	  #{@draw.created_at.strftime("%d/%m/%Y %H:%M")}
-Progreso: 	  #{Draw.progress(@draw.id)[:current]}%
----------------------------------
-Jugadas: #{@place.place_numbers.length} 	  Total: #{@place.place_numbers.length * @draw.price_unit}0$
----------------------------------
-#{@place.client != nil ? "            CLIENTE
----------------------------------
-Nombre:		  Javier Diaz
-Cedula:		  V-29.543.140
-Telefono:	  0412-1688466
----------------------------------" : nil}
-#{ApplicationRecord.generate_qr("http://localhost:3000/tickets?draw_id=#{@draw.id}&plays=#{@place.id}")}
-"
+    place_numbers = @place.place_numbers.to_s.tr('[]', '')
+  
+    qr_code_url = "http://localhost:3000/tickets?draw_id=#{@draw.id}&plays=#{@place.id}"
+    qr_code = "IMAGE|150|150|#{ApplicationRecord.generate_qr(qr_code_url)}"
+  
+@eighty_mm = <<-PLAIN_TEXT
+                   RIFAMAX\n------------------------------------------------\n                   NUMEROS\n#{place_numbers}\n------------------------------------------------\n                   PREMIOS\n#{@draw.first_prize}\n------------------------------------------------\nPrecio:    	      	      #{@draw.price_unit}0$\nTipo:    	      	      Terminal(00-99)\nAgencia:    	      	      #{@agency.name}\nTicket numero:    	      #{@draw.numbers}\nFecha de venta:    	      #{@place.created_at.strftime("%d/%m/%Y %H:%M")}\nTipo sorteo:    	              #{@draw.draw_type}\nFecha sorteo:    	      #{@draw.created_at.strftime("%d/%m/%Y %H:%M")}\nProgreso:    	      	      #{Draw.progress(@draw.id)[:current]}%\n------------------------------------------------\nJugadas: #{@place.place_numbers.length}    	      	      Total: #{@place.place_numbers.length * @draw.price_unit}0$\n------------------------------------------------#{@client.present? ? "\n                   CLIENTE\n------------------------------------------------\nNombre:    	      	      #{@client.name}\nCedula:    	      	      #{@client.dni}\nTelefono:    	      	      #{@client.phone}\n------------------------------------------------\n\n\n\n\n" : "\n\n\n\n\n"}
+PLAIN_TEXT
 
-    @fifty_six_mm = "
-           RIFAMAX
---------------------------------
-           NUMEROS:
-#{@place.place_numbers.to_s.tr('[]', '*****')}
---------------------------------
-Premio:        #{@draw.first_prize}
-Precio:        #{@draw.price_unit}0$
-Tipo:	          Terminal(00-99)
-Agencia:       #{@agency.name}
-Ticket num:    #{@draw.numbers}
-Fecha venta:   #{@place.created_at.strftime("%d/%m/%Y %H:%M")}
-Tipo sorteo:   #{@draw.draw_type}
-Fecha:	         #{@draw.created_at.strftime("%d/%m/%Y %H:%M")}
-Progreso:      #{Draw.progress(@draw.id)[:current]}%
---------------------------------
-Jugadas: #{@place.place_numbers.length}      Total: #{@place.place_numbers.length * @draw.price_unit}0$
---------------------------------
-#{@place.client != nil ? "          CLIENTE
---------------------------------
-Nombre:	        Javier Diaz
-Cedula:	        V-29.543.140
-Telefono:      0412-1688466
---------------------------------" : nil}
-#{ApplicationRecord.generate_qr("http://localhost:3000/tickets?draw_id=#{@draw.id}&plays=#{@place.id}")}
------------- CUT ---------------"
-
+@fifty_eight_mm = <<-PLAIN_TEXT
+RIFAMAX\n------------------------------------------------\n		          NUMEROS\n60\n------------------------------------------------\n		          PREMIOS\n10000$\n------------------------------------------------\nPrecio:    	      	      1.00$\nTipo:    	      	      Terminal(00-99)\nAgencia:    	      	      4 Bocas\nTicket numero:    	      633\nFecha de venta:    	      20/06/2023 09:49\nTipo sorteo:    	      	      Progressive\nFecha sorteo:    	      19/06/2023: 11:28\nProgreso:    	      	      60.0%\n------------------------------------------------\nJugadas: 1    	      	      Total: 1.00$\n-----------------------------------------------\n		          CLIENTE\n------------------------------------------------\nNombre:    	      	      Javier Diaz\nCedula:    	      	      V-29543140\nTelefono:    	      	      0412-1688466\n------------------------------------------------\n\n\n\n\n
+PLAIN_TEXT
+  
     if params[:print] == "80mm"
       render plain: @eighty_mm
     else
-      render plain: @fifty_six_mm
+      render plain: @fifty_eight_mm
     end
   end
-
+  
   private
 
   def place_params
