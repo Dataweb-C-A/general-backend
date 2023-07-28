b5a96ef17b39ede27329004add48ff28acf1f7e0
Javier Diaz <javierdiazt406@gmail.com>
2023-07-28 13:45:10 -0400

    Fix validations of messages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -35,8 +35,11 @@ class PlacesController < ApplicationController
     return unless place_params[:agency_id]
     if place_params[:user_id].present? && Draw.validate_draw_access(place_params[:user_id], request.headers[:Authorization])
       if place_params[:agency_id] && Whitelist.find_by(user_id: place_params[:agency_id])
-        GenerateDrawPlacesJob.new.sell_places(place_params[:draw_id], place_params[:place_nro], place_params[:agency_id])
-        render json: { place: Place.last, redirect: "https://#{ENV["HOST"]}/tickets?place_position=#{place_params[:place_nro]}" }, status: :ok
+        GenerateDrawPlacesJob.new.sell_places(place_params[:draw_id], place_params[:place_nro], place_params[:agency_id])[:completed] ? (
+          render json: { place: Place.last, redirect: "https://#{ENV["HOST"]}/tickets?place_position=#{place_params[:place_nro]}" }, status: :ok
+        ) : (
+          render json: { error: 'El lugar ya está vendido.', completed: false, status: 401 }, status: :unprocessable_entity
+        )
       else
         render json: { error: 'Unauthorized!', code: 401 }, status: :unauthorized
       end
