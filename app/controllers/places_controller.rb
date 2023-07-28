2f0c4e7004aebe5133837c7e401a45a67467a543
Javier Diaz <javierdiazt406@gmail.com>
2023-07-28 13:49:40 -0400

    Changing

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -36,6 +36,7 @@ class PlacesController < ApplicationController
     if place_params[:user_id].present? && Draw.validate_draw_access(place_params[:user_id], request.headers[:Authorization])
       if place_params[:agency_id] && Whitelist.find_by(user_id: place_params[:agency_id])
         GenerateDrawPlacesJob.new.sell_places(place_params[:draw_id], place_params[:place_nro], place_params[:agency_id])[:completed] ? (
+          GenerateDrawPlacesJob.new.sell_places(place_params[:draw_id], place_params[:place_nro], place_params[:agency_id])
           render json: { place: Place.last, redirect: "https://#{ENV["HOST"]}/tickets?place_position=#{place_params[:place_nro]}" }, status: :ok
         ) : (
           render json: { error: 'El lugar ya está vendido.', completed: false, status: 401 }, status: :unprocessable_entity
