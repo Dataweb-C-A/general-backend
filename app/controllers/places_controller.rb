e5e04d4eb7fce347237a7a54921ae35ab8529434
Javier Diaz <javierdiazt406@gmail.com>
2023-08-09 21:10:49 -0400

    Print module fixed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -67,7 +67,7 @@ class PlacesController < ApplicationController
   def print_text
     @draw = Draw.find(params[:draw_id])
     @place = Place.find(params[:plays])
-    @agency = Whitelist.find_by(user_id: agency_id)
+    @agency = Whitelist.find_by(user_id: @place.agency_id)
     #@client = Client.find(@place.client_id)
     
     place_numbers = @place.place_numbers.to_s.tr('[]', '')
