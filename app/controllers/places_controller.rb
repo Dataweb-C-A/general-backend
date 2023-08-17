653bd3d693fad36d587c4edfd9370de01d5cd272
Javier Diaz <javierdiazt406@gmail.com>
2023-08-17 13:05:57 -0400

    Test

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -101,12 +101,12 @@ class PlacesController < ApplicationController
     @draw = Draw.find(params[:draw_id])
     @agency = Whitelist.find_by(user_id: params[:agency_id])
 
-    place_numbers = params[:plays].to_s.tr('[]', ' ')
+    place_numbers = params[:plays].to_s.tr('[]', '').tr(',', ' ')
 
-    atributos_array = place_numbers.split(',')
+    atributos_array = place_numbers.split(' ')
 
 @eighty_mm = <<-PLAIN_TEXT
-                   RIFAMAX\n------------------------------------------------\n                   NUMEROS\n#{place_numbers}\n------------------------------------------------\n                   PREMIOS\n#{@draw.first_prize}\n------------------------------------------------\nPrecio:    	      	      10$\nTipo:    	      	      50-50\nAgencia:    	      	      #{@agency.name}\nTicket numero:    	      #{@draw.numbers}\nFecha de venta:    	      #{DateTime.now.strftime("%d/%m/%Y %H:%M")}\nFecha sorteo:    	      #{@draw.created_at.strftime("%d/%m/%Y %H:%M")}\n------------------------------------------------\nJugadas: #{atributos_array.length}    	      	      Total: #{atributos_array.length * 10}$\n------------------------------------------------#{false ? "\n                   CLIENTE\n------------------------------------------------\nNombre:    	      	      #{@client.name}\nCedula:    	      	      #{@client.dni}\nTelefono:    	      	      #{@client.phone}\n------------------------------------------------\n" : "\n\n\n\n\n"}
+                   RIFAMAX\n------------------------------------------------\n                   NUMEROS\n#{atributos_array.each do |a| if(a.to_i <= 999) "#{a}0 " else "#{a} " end}\n------------------------------------------------\n                   PREMIOS\n#{@draw.first_prize}\n------------------------------------------------\nPrecio:    	      	      10$\nTipo:    	      	      50-50\nAgencia:    	      	      #{@agency.name}\nTicket numero:    	      #{@draw.numbers}\nFecha de venta:    	      #{DateTime.now.strftime("%d/%m/%Y %H:%M")}\nFecha sorteo:    	      #{@draw.created_at.strftime("%d/%m/%Y %H:%M")}\n------------------------------------------------\nJugadas: #{atributos_array.length}    	      	      Total: #{atributos_array.length * 10}$\n------------------------------------------------#{false ? "\n                   CLIENTE\n------------------------------------------------\nNombre:    	      	      #{@client.name}\nCedula:    	      	      #{@client.dni}\nTelefono:    	      	      #{@client.phone}\n------------------------------------------------\n" : "\n\n\n\n\n"}
 PLAIN_TEXT
 
     render plain: @eighty_mm
